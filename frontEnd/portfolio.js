7d677b28adc5d3ef5e6e002ccb2dece1e7ba82c7
Derald Abbe <deraldabbe1@gmail.com>
2023-07-06 19:38:59 -0600

    added touch up to css files

--- frontEnd/portfolio.js
+++ frontEnd/portfolio.js
@@ -1,32 +1,155 @@
-// DOM elements
+
 const menuItems = document.querySelectorAll('.menu-item');
 const socialLinks = document.querySelectorAll('.social-link');
 
-// Function to handle menu item click
+
 function handleMenuItemClick(event) {
-  // Remove active class from all menu items
+
   menuItems.forEach((menuItem) => {
     menuItem.classList.remove('active');
   });
 
-  // Add active class to the clicked menu item
+
   event.target.classList.add('active');
 }
 
-// Function to handle social link click
+
 function handleSocialLinkClick(event) {
-  // Get the clicked social link URL
+
   const url = event.target.href;
 
-  // Open the URL in a new tab/window
+
   window.open(url, '_blank');
 }
 
-// Event listeners
+
 menuItems.forEach((menuItem) => {
   menuItem.addEventListener('click', handleMenuItemClick);
 });
 
 socialLinks.forEach((socialLink) => {
   socialLink.addEventListener('click', handleSocialLinkClick);
+});
+
+
+function setActiveLink() {
+  const currentPage = window.location.pathname;
+  const navLinks = document.querySelectorAll("nav ul li a");
+
+  navLinks.forEach((link) => {
+    if (link.getAttribute("href") === currentPage) {
+      link.classList.add("active");
+    } else {
+      link.classList.remove("active");
+    }
+  });
+}
+
+
+function addNavListeners() {
+  const navLinks = document.querySelectorAll("nav ul li a");
+
+  navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      const href = link.getAttribute("href");
+      if (href.startsWith("#")) {
+        scrollToSection(href);
+      }
+    });
+  });
+}
+
+
+function scrollToSection(href) {
+  const target = document.querySelector(href);
+  if (target) {
+    window.scrollTo({
+      top: target.offsetTop,
+      behavior: "smooth",
+    });
+  }
+}
+
+
+function initPortfolio() {
+  setActiveLink();
+  addNavListeners();
+}
+
+
+document.addEventListener("DOMContentLoaded", initPortfolio);
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  
+
+  const backButton = document.querySelector('.back-button');
+  backButton.addEventListener('click', () => {
+    window.location.href = 'index.html';
+  });
+  
+
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  
+  
+
+  const backButton = document.querySelector('.back-button');
+  backButton.addEventListener('click', () => {
+    window.location.href = 'index.html';
+  });
+  
+ 
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  
+
+  const backButton = document.querySelector('.back-button');
+  backButton.addEventListener('click', () => {
+    window.location.href = 'index.html';
+  });
+  
+  
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+ 
+  
+ 
+  const backButton = document.querySelector('.back-button');
+  backButton.addEventListener('click', () => {
+    window.location.href = 'index.html';
+  });
+
+  const githubButton = document.querySelector('.github-button');
+  githubButton.addEventListener('click', () => {
+    window.location.href = 'https://github.com/deraldabbe/deraldabbe.github.io.git';
+  });
+  
+  
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  
+ 
+  const form = document.querySelector('#contact-form');
+  form.addEventListener('submit', event => {
+    event.preventDefault();
+    
+ 
+    
+    
+    form.reset();
+  });
+  
+
 });
--- server/server.js
+++ server/server.js
@@ -5,6 +5,25 @@ const app = express();
 app.use(cors());
 app.use(express.static('frontEnd'));
 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+  });
+  
+  app.get('/about', (req, res) => {
+    res.sendFile(__dirname + '/about.html');
+  });
+  
+  app.get('/skills', (req, res) => {
+    res.sendFile(__dirname + '/skills.html');
+  });
+  
+  app.get('/projects', (req, res) => {
+    res.sendFile(__dirname + '/projects.html');
+  });
+  
+
+
+
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
     console.log('Server is running on port ${port}');
